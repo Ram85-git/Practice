b8ecb8dd5d8c2b1cb67de7f64bcbb7db8517b554
Ram <126403725+Ram85-git@users.noreply.github.com>
2023-05-27 23:05:45 -0400

    first commit

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="all">
      <div>Time</div>
-     <p id="time">{{ timer }}</p>
+     <!-- <p id="time">{{ timer }}</p> -->
+     <p> {{ hr }} :{{ min }} : {{ sec }} : {{ mils }}</p>
 
       <Control title="Start" :control="start"/>
      <Control title="Stop" :control="stop"/>
@@ -15,11 +16,19 @@ import Control from './Control.vue';
 export default{
     name: `Timer`,
     components:{
-        Control
+        Control,
+
+
+
     },
     data(){
         return{
-            timer:0
+            timerId:0,
+            timer:0,
+            mils:0,
+            sec:0,
+            min:0,
+            hr:0
         }
     },
     methods:{
@@ -28,8 +37,24 @@ export default{
         },
         start(){
             this.timerId=setInterval(()=>{
-                this.timer++;
-            },1000 );
+                this.mils++
+                if(this.mils==100){
+                    this.sec++;
+                   this.mils=0;
+                }
+                if(this.sec==60){
+                    this.min++;
+                    this.sec=0;
+                }
+                if(this.min==60){
+                    this.hr++;
+                    this.sec=0;
+                }
+
+
+
+
+            },100);
         },
         stop(){
             clearInterval(this.timerId);
@@ -47,12 +72,13 @@ export default{
 
 </script>
 <style scoped>
+
 .all{
     background-color: rgb(0, 217, 255);
     height: 200px;
     width: 200px;
     margin: auto;
-    padding: 50px;
+    padding-top: 50px;
     border: 10px solid rgb(219, 48, 114);
     border-radius: 20%;
 
